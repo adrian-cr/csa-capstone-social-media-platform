9e3dbd578d897cee362e6868f663324fda36cbb8
adrian-cr <acr77frb@gmail.com>
2025-05-19 00:59:16 -0600

    Added data modification, advanced query script files.

---
+++ scripts/queries_2.sql
@@ -0,0 +1,22 @@
+-- Add new post by user with ID 4 to the platform:
+INSERT INTO Posts (user_id, post_text, media_url, post_date)
+    VALUES (4, 'This is a new post!', 'https://picsum.photos/200', NOW());
+
+
+-- Add comment by user with ID 28 to post with ID 31:
+INSERT INTO Comments (post_id, user_id, comment_text, comment_date)
+    VALUES (31, 28, 'This is a comment on the post!', NOW());|
+
+-- Update profile information of user with ID 12:
+UPDATE Users
+    SET
+    username = 'new_username1',
+    email = 'new_email@gmail.com',
+    password = 'newP4$$wrd',
+    profile_picture_url = 'https://example.com/new_profile.jpg'
+WHERE user_id = 12;
+
+
+-- Remove like by user with ID 81 from post with ID 90:
+DELETE FROM Likes
+WHERE post_id = 90 AND user_id = 81;
---
+++ scripts/queries_3.sql
@@ -0,0 +1,29 @@
+-- Identify users with the most followers:
+SELECT 
+    u.username,
+    COUNT(*) AS follower_count
+FROM Users u LEFT JOIN Follows f
+    ON u.user_id = f.following_id
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10;
+
+
+-- Find the most active users based on post count and interaction:
+SELECT 
+    u.username,
+    (COUNT(DISTINCT p.post_id) + COUNT(DISTINCT c.comment_id) + COUNT(DISTINCT l.like_id)) AS total_activity,
+    COUNT(DISTINCT p.post_id) AS post_count,
+    COUNT(DISTINCT c.comment_id) AS comment_count,
+    COUNT(DISTINCT l.like_id) AS like_count
+FROM Users u LEFT JOIN Posts p
+	ON u.user_id = p.user_id LEFT JOIN Comments c
+		ON u.user_id = c.user_id LEFT JOIN Likes l
+			ON u.user_id = l.user_id
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- Calculate the average number of comments per post (100 comments / 100 posts = 1cpp):
+SELECT ROUND(COUNT(*) / (SELECT COUNT(*) FROM Posts), 0) AS avg_comments_per_post
+FROM Comments;
